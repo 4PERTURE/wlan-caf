ca57895c9d437ba4c4bd9461e01453fdccee874b
4PERTURE <slicey2001@gmail.com>
2019-09-06 13:28:58 +0000

    q stuff ig

--- qcwcn/wifi_hal/wifi_hal.cpp
+++ qcwcn/wifi_hal/wifi_hal.cpp
@@ -1365,11 +1365,8 @@ cleanup:
 }
 
 wifi_error wifi_start_sending_offloaded_packet(wifi_request_id id,
-                                               wifi_interface_handle iface,
-                                               u8 *ip_packet,
-                                               u16 ip_packet_len,
-                                               u8 *src_mac_addr,
-                                               u8 *dst_mac_addr,
+                                               wifi_interface_handle iface, u16 ether_type, u8 *ip_packet,
+                                               u16 ip_packet_len, u8 *src_mac_addr, u8 *dst_mac_addr,
                                                u32 period_msec)
 {
     wifi_error ret;
